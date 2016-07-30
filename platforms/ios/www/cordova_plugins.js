39be51d07e9ce48112cfb54ff94ef64e3479f33f
Lance Gleason <lgleasain@yahoo.com>
2016-07-30 19:57:56 -0400

    adding in skeleton stuff for beacons and some connect

--- platforms/ios/www/cordova_plugins.js
+++ platforms/ios/www/cordova_plugins.js
@@ -7,13 +7,31 @@ module.exports = [
         "clobbers": [
             "metawear.mwdevice"
         ]
+    },
+    {
+        "file": "plugins/cordova-plugin-ble/ble.js",
+        "id": "cordova-plugin-ble.BLE",
+        "pluginId": "cordova-plugin-ble",
+        "clobbers": [
+            "evothings.ble"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-eddystone/js/eddystone-plugin.js",
+        "id": "cordova-plugin-eddystone.eddystoneplugin",
+        "pluginId": "cordova-plugin-eddystone",
+        "clobbers": [
+            "evothings.eddystone"
+        ]
     }
 ];
 module.exports.metadata = 
 // TOP OF METADATA
 {
     "cordova-plugin-whitelist": "1.2.2",
-    "com.mbientlab.metawear": "0.2.15"
+    "com.mbientlab.metawear": "0.2.15",
+    "cordova-plugin-ble": "1.4.3",
+    "cordova-plugin-eddystone": "1.3.0"
 }
 // BOTTOM OF METADATA
 });
